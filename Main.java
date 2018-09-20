c4e4249157f7cc3dc41f289f143e3de9fb3db76e
HuangYH <huangyh@huangyh.top>
2018-09-20 20:44:34 +0800

    程序入口

---
+++ Main.java
@@ -0,0 +1,22 @@
+package Main;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		GetMessage gm = new GetMessage();
+		for (int i = 0; i < 1; i--) {
+			String sc_a = sc.nextLine();
+			if (sc_a.length() == 5) {
+				System.out.println(gm.GetMessage(sc_a));
+			} else {
+				System.out.println("输入错误，格式为栋数+宿舍号，如11510");
+			}
+		}
+		
+		
+	}
+
+}
