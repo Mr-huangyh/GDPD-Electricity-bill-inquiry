008989e36316947233bacacc7cd6f3da6d3b658e
HuangYH <huangyh@huangyh.top>
2018-09-20 20:46:16 +0800

    子程序

---
+++ GetMessage.java
@@ -0,0 +1,197 @@
+package Main;
+
+import java.net.*;
+
+public class GetMessage {
+	public String GetMessage(String i) {
+		String c1,c2,ancode1,ancode2;
+		c1 = i.substring(0,2);
+		c2 = i.substring(2,3);
+		
+		
+		ancode1 = getDNumber(c1);
+		if (ancode1.equals("")) {
+			return "宿舍不存在1  "+c1;
+		}else {
+			ancode2 = getSNumber(c2);
+			if (ancode2.equals("")) {
+				return "宿舍不存在2  "+c2;
+			} else {
+				String Gcut[] = Cut(Post(ancode1,i));
+				String Getcut = "";
+				for (int j = 0; j < Gcut.length; j++) {
+					switch (j) {
+					case 0:
+						Getcut += "宿舍号：";
+						break;
+					case 1:
+						Getcut += " 使用电量：";
+						break;
+					case 2:
+						Getcut += " 剩余电量：";
+						break;
+					case 3:
+						Getcut += " 时间：";
+						break;
+					}
+					Getcut += Gcut[j];
+				}
+				return Getcut;
+			}
+		}
+
+	}
+	private String getDNumber(String a) {
+		switch (a) {
+		case "10":
+			return"48";
+			
+		case "11":
+			return"39";
+			
+		case "12":
+	        return"1";
+
+		case "13":
+	        return"10";
+
+		case "14":
+	        return"18";
+
+		case "15":
+			return"32";
+	
+		case "16":
+			return"56";
+			
+		case "17":
+	        return"63";
+	        
+		case "18":
+	        return"70";
+
+		case "19":
+	        return"77";
+
+		case "20":
+	        return"88";
+
+		case "21":
+	        return"95";
+
+		case "22":
+	        return"102";
+
+		case "23":
+	        return"109";
+
+		case "24":
+	        return"116";
+
+		case "27":
+	        return"84";
+
+		case "28":
+	        return"123";
+
+		case "2":
+			return"149";
+
+		case "34":
+	        return"185";
+
+		case "40":
+	        return"157";
+
+		case "43":
+	        return"158";
+
+		case "45":
+	        return"132";
+
+		case "46":
+	        return"133";
+
+		case "47":
+	        return"159";
+
+		case "48":
+	        return"160";
+			
+		default:
+			return "";
+		}
+	}
+	private String getSNumber(String a) {
+		switch (a) {
+
+		case "1":
+			return "50";
+		
+		case "2":
+			return "51";
+		
+		case "3":
+			return "52";
+		
+		case "4":
+			return "53";
+		
+		case "5":
+			return "54";
+		
+		case "6":
+			return "55";
+
+		default:
+			return"";
+			
+		}
+	}
+	private String Post(String a,String b) {
+	Post request = new Post();
+	try{
+        //Proxy proxy = request.setProxy(InetAddress.getLocalHost().getHostAddress() , 8888 );
+        //System.out.println(proxy );
+        
+        String url ="http://dian.gdlgxy.com/RoomSelect/GetRoomInfo?";
+        String formData = "Apartid="+ a +"&Roomname=" + b;
+        String rs = request.sendPost(url, null , formData );
+        //System.out.println(a);
+        //System.out.println(b);
+        byte[] rs_bytecode = rs.getBytes();
+        String rs_encode = new String(rs_bytecode,"UTF-8");
+        return rs_encode;
+    }catch(Exception e ){
+        e.printStackTrace();
+        return "";
+    }
+	
+	}
+	private String[] Cut(String A) {
+		A = A.replace("\r", "");
+		A = A.replace("\n", "");
+		A = A.replace(" ", "");
+		System.out.println(A);
+		A = A.replaceAll("[?]", "c");
+		System.out.println(A);
+		A = A.replace("星期", " ");
+		
+		String[] C = A.split("<labelclass=\"infolab\">|</label>");
+		String[] Chun = new String[4];
+		//System.out.println(A);
+		int j = 0;
+		for (int i = 0; i < C.length-1; i++) {
+			if(i%2 == 0) {
+				Chun[j++] = C[i+1];
+			}
+		}
+		
+		//for (int i = 0; i < Chun.length; i++) {
+			//System.out.println(Chun[i]);
+		//}
+		//System.out.println(Chun.length);
+		return Chun;
+	}
+}
+
---
+++ Post.java
@@ -0,0 +1,62 @@
+package Main;
+
+import java.io.*;
+import java.net.*;
+
+public class Post {
+
+    public Proxy setProxy(String proxyIp , int proxyPort ){
+        try{
+            InetSocketAddress socketAddress 
+                = new InetSocketAddress(proxyIp , proxyPort );
+            Proxy proxy = new Proxy(Proxy.Type.HTTP , socketAddress );
+            return proxy;
+        }catch(Exception e ){
+            e.printStackTrace();
+        }
+        return null;
+    }
+    
+    public String sendPost(String url , String params  , String formData) throws Exception{
+
+        StringBuilder builder = new StringBuilder();
+
+        if(!(params == null || params.length() == 0) ){
+            url += ("?" + params ); 
+        }
+
+        URL Url = new URL(url );
+        URLConnection conn = Url.openConnection();
+
+        //如果设置代理 , 和发送GET一样.
+        conn.setRequestProperty("accept", "*/*" );
+        conn.setRequestProperty("Connection", "Keep-Alive" );
+        conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.86 Safari/537.36");
+
+        //设置之后就可以发送POST请求了
+        conn.setDoInput(true );
+        conn.setDoOutput(true );
+
+
+        //获取它的输出流 , 直接写入post请求
+        PrintWriter writer = new PrintWriter(conn.getOutputStream() );
+        writer.print(formData );                                    
+        writer.flush();
+
+
+        //获取浏览器的返回数据
+        BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream() ) );
+        String line = reader.readLine();
+        line = new String(line.getBytes() , "utf-8" );  //解决乱码的问题
+        while(line != null ){
+            //System.out.println(line );
+            builder.append(line + "\r\n" );
+            line = reader.readLine();
+        }
+        reader.close();
+        writer.close();
+
+
+        return builder.toString();
+    }
+}
